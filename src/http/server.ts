88354a06e8e40aef9517cfbe4321a305dcb7bf75
Victor Feldhaus <victorm.feldhaus@gmail.com>
2024-02-08 20:30:56 -0300

    feat: finalizando

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,24 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client"
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { createPoll } from "./routes/create_poll";
+import { getPoll } from "./routes/get_poll";
+import { voteOnPoll } from "./routes/vote_on_poll";
+import { pollResults } from "./ws/poll_results";
  
 const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(cookie, {
+    secret: "polls-secret",
+    hook: "onRequest",
+});
 
-app.post("/polls", async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
+app.register(websocket)
 
-    const { title } = createPollBody.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-
-    return reply.status(201).send({pollId: poll.id})
-})
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server is running!☄️")
